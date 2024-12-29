83bf44cac28d04cae15b0beb4f8f5013bd17080a
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2024-12-29 11:10:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,12 +29,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "17000"))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001511253627"))
+LOGGER_ID = int(getenv("LOGGER_ID", "RICHA_FIGHTER"))
 
-LOG_GROUP_ID = int(getenv("LOGGER_ID", "-1001511253627"))
+LOG_GROUP_ID = int(getenv("LOGGER_ID", "RICHA_FIGHTER)
 
-# Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7708051264"))
+# Get this value from @NOBITA_ALL_BOT on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", "6972508083"))
 
 
 ## Fill these variables if you're deploying on heroku.
